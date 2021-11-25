b75d38ad121532f69a359d60c1f9284cd1cbca04
bartektelec <bartektelec@gmail.com>
2021-11-25 23:42:34 +0100

    added logic to card component

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,14 +1,55 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
-export class CardComponent implements OnInit {
+export class CardComponent implements OnInit, OnChanges {
   @Input() imgSrc!: string;
   @Input() imgAlt!: string;
+  currentIndex = 0;
+  filledOutLetters = [];
+  splitLetters = [];
+  isShaking = false;
   constructor() {}
 
-  ngOnInit() {}
+  getMixedLetters = () =>
+    this.imgAlt.split('').sort(() => (Math.random() > 0.5 ? -1 : 1));
+
+  clickLetter = (e: PointerEvent) => {
+    const target = e.target as HTMLDivElement;
+    console.log(this.imgAlt[this.currentIndex]);
+    console.log(target.innerText);
+
+    if (this.imgAlt[this.currentIndex] !== target.innerText.toLowerCase()) {
+      this.shakeScreen();
+      return;
+    }
+
+    this.currentIndex += 1;
+    this.filledOutLetters = this.imgAlt
+      .split('')
+      .map((letter, i) => (i < this.currentIndex ? letter : null));
+  };
+
+  shakeScreen = () => {
+    this.isShaking = true;
+    setTimeout(() => {
+      this.isShaking = false;
+    }, 500);
+  };
+
+  ngOnChanges(changes) {
+    console.log(changes);
+  }
+
+  ngOnInit() {
+    this.filledOutLetters = this.imgAlt.split('').map((x) => null);
+    this.splitLetters = this.imgAlt.split('');
+
+    while (this.splitLetters.join('') === this.imgAlt) {
+      this.splitLetters = this.getMixedLetters();
+    }
+  }
 }
